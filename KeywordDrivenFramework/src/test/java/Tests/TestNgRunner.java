c4bee27afe614db0c4893c684df6f50e3aa8a346
Lakshmi Pathi <Lakshmi.Pathi@ATMECS.CORP>
2023-06-21 14:42:41 +0530

    third commit

--- KeywordDrivenFramework/src/test/java/Tests/TestNgRunner.java
+++ KeywordDrivenFramework/src/test/java/Tests/TestNgRunner.java
@@ -1,18 +1,19 @@
 package Tests;
 
-import org.testng.annotations.Test;
 
+import org.testng.annotations.Test;
 import keywords.AdminPageKeywords;
 import keywords.LoginPageKeyword;
 
 public class TestNgRunner {
 	LoginPageKeyword logkey = new LoginPageKeyword();
 	AdminPageKeywords adminkey = new AdminPageKeywords();
-	
 	Action action = new Action();
 	
-	@Test(priority=1)
-	public void executeLoginPage() {
+	
+
+	@Test
+	public void executeTest() {
 		
 		try {
 			action.execute_Test("Login_testdata",logkey);
@@ -21,15 +22,6 @@ public class TestNgRunner {
 		}
 		
 	}
-	@Test(priority=2)
-       public void executeAdminPage() {
-		
-		try {
-			action.execute_Test("Admin_facet",adminkey);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-	}
-
+	
+	
 }
--- KeywordDrivenFramework/src/test/java/keywords/LoginPageKeyword.java
+++ KeywordDrivenFramework/src/test/java/keywords/LoginPageKeyword.java
@@ -10,47 +10,45 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import Tests.Action;
 import utilities.ExcelUtility;
 
-public class LoginPageKeyword {
+public class LoginPageKeyword  {
 	
 	static WebDriver driver;
 	static WebElement element;
 	
-	public static void openbrowser() {
+	
+	public void openbrowser() {
 		System.setProperty("webdriver.chrome.driver", "/D:/drivers/chromedriver.exe");
 		driver = new ChromeDriver();
 	}
 	
 	@SuppressWarnings("deprecation")
-	public static void launchurl() {
+	public void launchurl() {
 		driver.navigate().to(ExcelUtility.DataValue);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
 	}
-	public static void verifyLoginPage() {
+	public void verifyLoginPage() {
 		element = driver.findElement(Action.locator);
 		element.isDisplayed();
 	}
 	
-	public static void enterusername() {
+	public void setText() {
 	 element = driver.findElement(Action.locator);
 	 element.sendKeys(ExcelUtility.DataValue);
 		
 	}
 	
-	public static void enterpassword() {
-		element = driver.findElement(Action.locator);
-		element.sendKeys(ExcelUtility.DataValue);
-	}
-	
-	public static void clicklogin() {
+	public static void click() {
 		element = driver.findElement(Action.locator);
 		element.click();
 	}
+	
     public void verifyhomepage() {
     	element = driver.findElement(Action.locator);
     	String actual = element.getText();
     	assertEquals(actual, "Dashboard");
     	System.out.println("Logged into the home page of orange hrm");
     }
+    
     
 }
